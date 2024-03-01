e74414c88fe6b3eafda7b0725c20928bce79e6ad
citraFebriawirti <febriawirticitra@gmail.com>
2024-03-01 22:57:49 +0700

    Feat: add page kalas

--- resources/views/Layouts_LandingPage/layout.blade.php
+++ resources/views/Layouts_LandingPage/layout.blade.php
@@ -765,7 +765,7 @@
                         href="#"
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-main rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       >
-                        Baca
+                        Bacaaaaaaa
                       </a>
                     </div>
                   </div>
